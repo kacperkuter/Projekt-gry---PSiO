12438197d52706f1e18989e95cb02619e734dc2d
kacperkuter <112434822+kacperkuter@users.noreply.github.com>
2026-06-08 13:19:26 +0200

    Add files via upload

--- include/gracz.h
+++ include/gracz.h
@@ -9,6 +9,11 @@ private:
     sf::RectangleShape ksztalt;
     sf::Vector2f predkosc;
     float szybkosc_ruchu;
+    
+    // Zmienne fizyczne
+    bool czy_na_ziemi;
+    float grawitacja;
+    float sila_skoku;
 
     // Metoda wymagana przez sf::Drawable do rysowania gracza na ekranie
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
@@ -17,6 +22,14 @@ public:
     Gracz();
     void aktualizuj(float dt);
     void obsluz_sterowanie();
+
+    // Gettery i settery do fizyki i kolizji
+    sf::FloatRect pobierz_granice() const { return ksztalt.getGlobalBounds(); }
+    void ustaw_pozycje(float x, float y) { ksztalt.setPosition(x, y); }
+    sf::Vector2f pobierz_pozycje() const { return ksztalt.getPosition(); }
+    sf::Vector2f pobierz_predkosc() const { return predkosc; }
+    void wylacz_predkosc_y() { predkosc.y = 0.f; }
+    void ustaw_na_ziemi(bool na_ziemi) { czy_na_ziemi = na_ziemi; }
 };
 
 #endif
